fdc2c32e2fd494cd866e208c3f7988dea44ac9a9
junya uchizono <junya19991215@icloud.com>
2022-11-05 23:39:00 +0900

    fix qemu_exec.sh

--- bare_metal_practice/qemu-exec.sh
+++ bare_metal_practice/qemu-exec.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+export PATH=/usr/local/Cellar/dosfstools/4.2/sbin:$PATH
+export PATH=/usr/local/opt/llvm/bin:$PATH
+
 # ブートローダのビルド
 source ~/edk2/edksetup.sh
-~/edk2/Build
+~/edk2/build
 
 # カーネルのビルド
+source ~/osbook/devenv/buildenv.sh
 cd ~/workspace/mikanos/kernel
 make kernel.elf
 cd ~/
@@ -16,7 +20,7 @@ fi
 
 # FAT形式の擬似USB作成
 qemu-img create -f raw disk.img 200M
-mkfs.fat -n 'uchi os' -s 2 -f 2 -R 3 -F 32 disk.img
+mkfs.fat -n 'URUUNARI' -s 2 -f 2 -R 3 -F 32 disk.img
 hdiutil attach -mountpoint mnt disk.img
 
 # ブートローダ配置
@@ -29,6 +33,6 @@ cp ~/workspace/mikanos/kernel/kernel.elf mnt
 hdiutil detach mnt
 
 # 実行
-qemu-system-x86_64 -drive if=pflash,file=OVMF_CODE.fd -drive if=pflash,file=OVMF_VARS.fd -hda disk.img
+qemu-system-x86_64 -m 1G -drive if=pflash,format=raw,file=OVMF_CODE.fd -drive if=pflash,format=raw,file=OVMF_VARS.fd -drive if=ide,index=0,media=disk,format=raw,file=disk.img -device nec-usb-xhci,id=xhci -device usb-mouse -device usb-kbd 
 
 
